85ee1f05cbef3a6d64c3cb8f43ac04bb49d8520e
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2023-10-22 02:17:01 +0100

    modified hello/view.py for autograder purpose

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def Index(request):
     num_visits = request.session.get("num_visits", 0) + 1
     request.session["num_visits"] = num_visits
-    resp = HttpResponse(f'Hi, you have viewed this page {num_visits} times')
+    resp = HttpResponse(f'Hi, you have viewed this page {num_visits} times' +'<br>' + '# this line is for certification autograder view count='+str(num_visits))
     resp.set_cookie('dj4e_cookie', '60bbae9c', max_age=1000)
     return resp
