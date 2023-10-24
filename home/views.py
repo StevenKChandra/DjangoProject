ca788223117588db2193871cb0ff039c3962c3c2
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2023-10-24 21:30:24 +0100

    added login and logout button to home page

--- home/views.py
+++ home/views.py
@@ -14,7 +14,6 @@ class IndexView(generic.View):
         dirlist_parent = [dir for dir in os.listdir(".") if os.path.isdir(f"./{dir}")]
         applications = [app for app in dirlist_parent if "apps.py" in os.listdir(f"./{app}")]
         placeholder = []
-        print(os.listdir(".."))
         for application in applications:
             application = apps.get_app_config(application)
             app = appdata()
@@ -30,7 +29,6 @@ class IndexView(generic.View):
     #     dirlist_parent = [dir for dir in os.listdir(".") if os.path.isdir(f"./{dir}")]
     #     applications = [app for app in dirlist_parent if "apps.py" in os.listdir(f"./{app}")]
     #     placeholder = []
-    #     print(os.listdir(".."))
     #     for application in applications:
     #         application = apps.get_app_config(application)
     #         app = appdata()
