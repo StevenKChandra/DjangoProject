bc43394135dd8f3277d08cc04981790c422aa79c
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2023-10-24 21:14:20 +0100

    fixed homepage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from django.apps import apps
+from django.conf import settings
+from django.shortcuts import render
 import os
 
 class appdata():
@@ -7,12 +9,8 @@ class appdata():
         self.link = None
         self.name = None
 
-class IndexView(generic.ListView):
-    template_name = "home/main.html"
-    context_object_name = "applications"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
+class IndexView(generic.View):
+    def get(self, request):
         dirlist_parent = [dir for dir in os.listdir(".") if os.path.isdir(f"./{dir}")]
         applications = [app for app in dirlist_parent if "apps.py" in os.listdir(f"./{app}")]
         placeholder = []
@@ -23,4 +21,20 @@ class IndexView(generic.ListView):
             app.link = application.label+":index"
             app.name = application.verbose_name
             placeholder.append(app)
-        return placeholder
+        return render(request, "home/main.html", {"applications": placeholder, "web_name": settings.WEB_NAME})
+
+    # template_name = "home/main.html"
+    # context_object_name = "application"
+    # def get_queryset(self):
+    #     """Return the last five published questions."""
+    #     dirlist_parent = [dir for dir in os.listdir(".") if os.path.isdir(f"./{dir}")]
+    #     applications = [app for app in dirlist_parent if "apps.py" in os.listdir(f"./{app}")]
+    #     placeholder = []
+    #     print(os.listdir(".."))
+    #     for application in applications:
+    #         application = apps.get_app_config(application)
+    #         app = appdata()
+    #         app.link = application.label+":index"
+    #         app.name = application.verbose_name
+    #         placeholder.append(app)
+    #     return placeholder
