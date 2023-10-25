d3690d3996e7c23e9c25660df64d41d238282a6f
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2023-10-25 01:40:26 +0100

    added add comments

--- ads/urls.py
+++ ads/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = "ads"
 urlpatterns = [
     path("", views.AdListView.as_view(), name="index"),
-    path("<int:pk>/", views.AdDetailView.as_view(), name="detail"),
-    path("create/", views.AdCreateView.as_view(success_url=reverse_lazy("ads:index")), name="create"),
-    path("<int:pk>/update/", views.AdUpdateView.as_view(success_url=reverse_lazy("ads:index")), name="update"),
-    path("<int:pk>/delete/", views.AdDeleteView.as_view(success_url=reverse_lazy("ads:index")), name="delete"),
+    path("ad/<int:pk>/", views.AdDetailView.as_view(), name="detail"),
+    path("ad/create/", views.AdCreateView.as_view(success_url=reverse_lazy("ads:index")), name="create"),
+    path("ad/<int:pk>/update/", views.AdUpdateView.as_view(success_url=reverse_lazy("ads:index")), name="update"),
+    path("ad/<int:pk>/delete/", views.AdDeleteView.as_view(success_url=reverse_lazy("ads:index")), name="delete"),
+    path("comment/<int:pk>/create/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,14 +1,27 @@
-from ads.models import Ad
-from django.views.generic import ListView, DetailView
-from ads.owner import OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 
+from ads.owner import OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from ads.models import Ad, Comment
+from ads.forms import CommentForm
 # Create your views here.
 
-class AdListView(ListView):
+class AdListView(generic.ListView):
     model = Ad
 
-class AdDetailView(DetailView):
+class AdDetailView(generic.DetailView):
     model = Ad
+    template_name = "ads/ad_detail.html"
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk)
+        context = {}
+        context["ad"] = get_object_or_404(Ad, id=pk)
+        context["comments"] = Comment.objects.filter(ad=ad).order_by("-updated_at")
+        context["comment_form"] = CommentForm()
+
+        return render(request, self.template_name, context)
 
 class AdCreateView(OwnerCreateView):
     model = Ad
@@ -20,4 +33,18 @@ class AdUpdateView(OwnerUpdateView):
     
 class AdDeleteView(OwnerDeleteView):
     model = Ad
-    # fields = ["title", "price", "text"]
+
+class CommentCreateView(LoginRequiredMixin, generic.CreateView):
+    def post(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=ad)
+        comment.save()
+        
+        return redirect(reverse("ads:detail", args=[pk]))
+    
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('ads:detail', args=[ad.id])
